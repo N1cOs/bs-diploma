187cd3119fa43bd12d3981d5a14fab94bfbd3b83
Nikita Karmatskikh <nickkarmatskikh@gmail.com>
2021-03-23 03:14:52 +0300

    src: darknet: detector: add transforming input and scaling output coordinates

--- src/darknet/detector.py
+++ src/darknet/detector.py
@@ -1,8 +1,10 @@
 import dataclasses
-from typing import List
+from typing import List, Tuple
 
+import numpy as np
 import torch
 from torchvision import ops
+from torchvision import transforms as tr
 
 from . import network
 
@@ -12,17 +14,28 @@ class ObjectDetector:
         net.eval()
         self.net = net
         self.classes = classes
+        # ToDo: get value from network definition
+        self.img_shape = ImageShape(416, 416)
 
     def detect(
-        self, img: torch.Tensor, conf_threshold: float = 0.5, iou_threshold: float = 0.4
+        self, img: np.ndarray, conf_threshold: float = 0.5, iou_threshold: float = 0.4
     ) -> List["DetectionResult"]:
-        predictions = self.net(img)
+        transforms = tr.Compose(
+            [
+                tr.ToTensor(),
+                tr.Resize(self.img_shape.hw_tuple()),
+            ]
+        )
+        tr_img = transforms(img)
+
+        predictions = self.net(tr_img.unsqueeze(0))
         # from (center x, center y, width, height) to (x1, y1, x2, y2)
         predictions[..., :4] = self._xywh2xyxy(predictions[..., :4])
 
         results = []
+        orig_shape = ImageShape.from_numpy(img)
         for prediction in predictions:
-            # filter out confidence scores below threshold
+            # filter out object confidence below threshold
             prediction = prediction[prediction[:, 4] >= conf_threshold]
 
             coordinates = prediction[:, :4]
@@ -39,14 +52,16 @@ class ObjectDetector:
             )
 
             for obj in objects:
-                clazz_i = int(obj[5].item())
+                obj = [o.item() for o in obj]
+
+                clazz_i = int(obj[5])
                 try:
                     clazz = self.classes[clazz_i]
                 except IndexError:
                     raise ValueError(f"unknown class with index {clazz_i}")
 
                 res = DetectionResult(obj[0], obj[1], obj[2], obj[3], obj[4], clazz)
-                results.append(res)
+                results.append(self._rescaled_result(res, orig_shape))
 
         return results
 
@@ -59,6 +74,20 @@ class ObjectDetector:
         y[..., 3] = x[..., 1] + x[..., 3] / 2
         return y
 
+    def _rescaled_result(
+        self, res: "DetectionResult", orig_shape: "ImageShape"
+    ) -> "DetectionResult":
+        scale_x = orig_shape.width / self.img_shape.width
+        scale_y = orig_shape.height / self.img_shape.height
+
+        return dataclasses.replace(
+            res,
+            x1=res.x1 * scale_x,
+            y1=res.y1 * scale_y,
+            x2=res.x2 * scale_x,
+            y2=res.y2 * scale_y,
+        )
+
 
 @dataclasses.dataclass
 class DetectionResult:
@@ -68,3 +97,20 @@ class DetectionResult:
     y2: float
     score: float
     clazz: str
+
+
+@dataclasses.dataclass
+class ImageShape:
+    width: int
+    height: int
+
+    @classmethod
+    def from_numpy(cls, arr: np.ndarray):
+        try:
+            shape = arr.shape
+            return cls(shape[1], shape[0])
+        except IndexError:
+            raise ValueError("invalid numpy array: shape must be greater or equal to 2")
+
+    def hw_tuple(self) -> Tuple[int]:
+        return tuple([self.height, self.width])
