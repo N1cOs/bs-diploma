862f805a0978800a32d0e575e1d81a97060f7790
Nikita Karmatskikh <nickkarmatskikh@gmail.com>
2021-05-01 13:34:19 +0300

    data: get_model: add yolov4

--- src/data/get_model.sh
+++ src/data/get_model.sh
@@ -15,6 +15,10 @@ case $1 in
     wget 'https://drive.google.com/uc?id=1Ahmn7iI2B79jDlcKlRlD_tO5Tbr0Dq8x&export=download' -O "$1".cfg
     wget 'https://github.com/AlexeyAB/darknet/releases/download/darknet_yolo_v3_optimal/yolov4.weights'
     ;;
+  yolov4)
+    wget 'https://raw.githubusercontent.com/AlexeyAB/darknet/master/cfg/yolov4.cfg' -O "$1".cfg
+    wget 'https://github.com/AlexeyAB/darknet/releases/download/darknet_yolo_v3_optimal/yolov4.weights'
+    ;;
   yolov3)
     wget 'https://raw.githubusercontent.com/pjreddie/darknet/master/cfg/yolov3.cfg'
     wget 'https://pjreddie.com/media/files/yolov3.weights'
