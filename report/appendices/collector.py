629e19e6247b9708980c03b6ff490808751288e4
Nikita Karmatskikh <nickkarmatskikh@gmail.com>
2021-05-07 18:45:36 +0300

    report: add implementation

---
+++ report/appendices/collector.py
@@ -0,0 +1,178 @@
+import asyncio
+import heapq
+import logging
+import time
+from typing import List
+
+import zmq
+
+import proto
+import stats
+import video
+
+
+class DetectionCollector:
+  def __init__(
+    self,
+    sock: zmq.Socket,
+    read_buf: "AsyncDict",
+    read_all: asyncio.Event,
+    recv_buf_size: int,
+    write_buf_size: int,
+    frame_timeout: int,
+    classes: List[str],
+    write_label: bool,
+    frame_writer: video.AsyncFrameWriter,
+    stats_ctl: stats.FrameEventsCollector,
+  ):
+    self._sock = sock
+    self._read_buf = read_buf
+    self._read_all = read_all
+    self._recv_max_size = recv_buf_size
+    self._frame_timeout = frame_timeout
+
+    self._detection_writer = video.AsyncDetectionWriter(classes, write_label)
+    self._frame_writer = frame_writer
+    self._stats_ctl = stats_ctl
+
+    self._dropped = 0
+    self._write_queue = asyncio.Queue(maxsize=write_buf_size)
+    self._stopping = asyncio.Event()
+    self._log = logging.getLogger(__name__)
+
+  @property
+  def dropped_frames(self):
+    return self._dropped
+
+  def start(self) -> asyncio.Task:
+    write_task = asyncio.create_task(self._write_detections(), name="write_task")
+    return asyncio.create_task(self._poll_detections(write_task), name="poll_task")
+
+  async def _poll_detections(self, write_task: asyncio.Task):
+    name = asyncio.current_task().get_name()
+    self._log.info(f"{name}: started")
+    start_time = time.perf_counter()
+
+    pq = []
+    frames = {}
+
+    async def write_head():
+      head = heapq.heappop(pq)
+      frame = frames.pop(head.id)
+      self._log.debug(f"{name}: writing frame: id={head.id}")
+      await self._write_queue.put((frame, head.detections))
+      return head.id
+
+    async def check_drops(cur, prev):
+      if cur != prev + 1:
+        dropped = [i for i in range(prev + 1, cur)]
+        for i in dropped:
+          await self._read_buf.pop(i)
+
+        self._log.warning(f"{name}: dropping frame: ids={dropped}")
+        self._dropped += len(dropped)
+
+    last_written = -1
+    while True:
+      try:
+        raw_resp = await asyncio.wait_for(
+          self._sock.recv(), self._frame_timeout
+        )
+        resp = proto.parse_detect_response(raw_resp)
+        self._log.debug(f"{name}: received frame: id={resp.id}")
+        await self._stats_ctl.send_detect_duration(resp.elapsed_sec)
+
+        if resp.id <= last_written:
+          # just drop received frame
+          continue
+
+        heapq.heappush(pq, resp)
+        frames[resp.id] = await self._read_buf.pop(resp.id)
+
+        while pq and pq[0].id == last_written + 1:
+          await write_head()
+          last_written += 1
+
+        if len(pq) > self._recv_max_size:
+          # write nearest frame which we already received
+          id_ = await write_head()
+          await check_drops(id_, last_written)
+          last_written = id_
+      except asyncio.TimeoutError:
+        if not self._read_all.is_set():
+          self._log.warning(f"{name}: got frame timeout")
+          continue
+
+        while pq:
+          id_ = await write_head()
+          await check_drops(id_, last_written)
+          last_written = id_
+
+        elapsed = time.perf_counter() - start_time
+        elapsed = round(elapsed - self._frame_timeout, 2)
+        self._log.info(
+          f"{name}: process all frames, stopping: processing time={elapsed}s"
+        )
+
+        self._stopping.set()
+        await write_task
+        break
+      except asyncio.CancelledError:
+        self._log.info(f"{name}: got cancel signal, stopping")
+        break
+      except:
+        self._log.error(f"{name}: unhandled exception", exc_info=True)
+
+    self._log.info(f"{name}: finished")
+
+  async def _write_detections(self):
+    name = asyncio.current_task().get_name()
+    self._log.info(f"{name}: started")
+
+    async def write(res):
+      frame, detections = res
+      now = time.perf_counter()
+      for d in detections:
+        await self._detection_writer.write(frame, d)
+      await self._stats_ctl.send_write_detections_duration(
+        time.perf_counter() - now
+      )
+
+      now = time.perf_counter()
+      await self._frame_writer.write(frame)
+      await self._stats_ctl.send_encode_duration(time.perf_counter() - now)
+
+    async def write_until_empty(q):
+      while not q.empty():
+        res = await q.get()
+        await write(res)
+
+    wait_task = asyncio.create_task(self._stopping.wait())
+    async with self._frame_writer:
+      while True:
+        try:
+          write_task = asyncio.create_task(self._write_queue.get())
+          tasks = {wait_task, write_task}
+
+          done, _ = await asyncio.wait(
+            tasks, return_when=asyncio.FIRST_COMPLETED
+          )
+          if write_task in done:
+            res = write_task.result()
+            await write(res)
+          elif wait_task in done:
+            self._log.info(f"{name}: got stopping signal")
+            await write_until_empty(self._write_queue)
+            break
+          else:
+            raise ValueError(f"unknown done tasks: {done}")
+        except asyncio.CancelledError:
+          self._log.info(
+            f"{name}: got cancel signal, writing all buffered detections"
+          )
+          await write_until_empty(self._write_queue)
+          break
+        except:
+          self._log.error(f"{name}: unhandled exception", exc_info=True)
+
+    self._log.info(f"{name}: finished")
---
+++ report/appendices/detector.py
@@ -0,0 +1,134 @@
+import dataclasses
+import re
+from typing import List, Tuple
+
+import cv2
+import numpy as np
+
+
+@dataclasses.dataclass
+class DetectionResult:
+  x1: int
+  y1: int
+  x2: int
+  y2: int
+  score: float
+  clazz: int
+ 
+
+class DarknetObjectDetector:
+  _SCALE_FACTOR = 0.00392
+  _RGB_MEAN = (0, 0, 0)
+
+  def __init__(self, cfg: str, weights: str, target: int, warm_up: bool = True):
+    net = cv2.dnn.readNetFromDarknet(cfg, weights)
+    net.setPreferableTarget(target)
+
+    self._net = net
+    self._size = self._parse_net_size(cfg)
+    self._out_names = net.getUnconnectedOutLayersNames()
+
+    if warm_up:
+      shape = self._size + (3,)
+      dummy_img = np.random.randint(0, 255, shape, dtype=np.uint8)
+      self.detect(dummy_img)
+
+  def detect(
+    self, img: np.ndarray, conf_threshold: float = 0.5, nms_threshold: float = 0.4
+  ) -> List["DetectionResult"]:
+    blob = cv2.dnn.blobFromImage(
+      img,
+      self._SCALE_FACTOR,
+      self._size,
+      self._RGB_MEAN,
+      swapRB=True,
+      crop=False,
+    )
+    self._net.setInput(blob)
+    outs = self._net.forward(self._out_names)
+
+    layers = self._net.getLayerNames()
+    last_layer_id = self._net.getLayerId(layers[-1])
+    last_layer = self._net.getLayer(last_layer_id)
+
+    boxes = []
+    classes = []
+    confidences = []
+    img_h, img_w = img.shape[0], img.shape[1]
+    if last_layer.type == "Region":
+      for out in outs:
+        for detection in out:
+          scores = detection[5:]
+          clazz = np.argmax(scores)
+          confidence = scores[clazz]
+
+          if confidence > conf_threshold:
+            center_x, center_y = detection[0] * img_w, detection[1] * img_h
+            w, h = detection[2] * img_w, detection[3] * img_h
+            left, top = center_x - w / 2, center_y - h / 2
+
+            classes.append(clazz)
+            # important: confidence must be float
+            confidences.append(float(confidence))
+            # important: boxes must be ints
+            boxes.append(list(map(int, [left, top, w, h])))
+    else:
+      raise ValueError(f"unknown last layer type: {last_layer.type}")
+
+    indices = [i[0] for i in cv2.dnn.NMSBoxes(boxes, confidences, conf_threshold, nms_threshold)]
+    results = []
+    for i in indices:
+      box = boxes[i]
+      left, top = box[0], box[1]
+      w, h = box[2], box[3]
+
+      res = DetectionResult(
+        x1=left,
+        y1=top,
+        x2=left + w,
+        y2=top + h,
+        score=confidences[i],
+        clazz=classes[i],
+      )
+      results.append(res)
+
+    return results
+
+  @staticmethod
+  def _parse_net_size(cfg_path: str) -> Tuple[int, int]:
+    with open(cfg_path, "r") as cfg:
+      raw_sections = []
+
+      section = {}
+      header_re = re.compile(r"^\[(?P<name>\w+)\]$")
+      for line in cfg:
+        if line.startswith("#") or line.isspace():
+          continue
+
+        match = header_re.match(line)
+        if match:
+          if section:
+            raw_sections.append(section)
+          section = {"type": match.group("name")}
+        else:
+          key, val = [s.strip() for s in line.split("=")]
+          if key in section:
+            raise ValueError(
+              f"duplicate section key: section={section['type']}, key={key}"
+            )
+          section[key] = val
+      raw_sections.append(section)
+
+    for raw in raw_sections:
+      type_ = raw["type"]
+      if type_ == "net":
+        width = raw.get("width")
+        if width is None:
+          raise ValueError(f"missing width in net section: {cfg_path}")
+
+        height = raw.get("height")
+        if height is None:
+          raise ValueError(f"missing height in net section: {cfg_path}")
+        
+        return int(width), int(height)
+    raise ValueError(f"missing net section: {cfg_path}")
